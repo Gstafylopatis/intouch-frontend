4b529387b8727261473c04cb41d8ff5b55c34a80
gstafylopatis <gstafylopatis@gmail.com>
2021-11-05 14:52:09 +0200

    First Commit Added Clients Table with some hardcoded data

---
+++ src/components/Clients.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class Clients extends Component {
+  state = {
+    clients: [
+      {
+        id: 1,
+        name: 'george',
+        tax_id: '123213213',
+        address: 'latamias 38',
+        phone: '123123',
+      },
+      {
+        id: 2,
+        name: 'antonis',
+        tax_id: '232323232',
+        address: 'latamias 42',
+        phone: '657897',
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Tax ID</th>
+            <th>Address</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.clients.map((client) => (
+            <tr key={client.id}>
+              <td>{client.name}</td>
+              <td>{client.tax_id}</td>
+              <td>{client.address}</td>
+              <td>{client.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Clients;
