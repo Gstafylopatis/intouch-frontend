6152b4d21343ff073adfcc10b4f3d8fa3eeadcf1
gstafylopatis <gstafylopatis@gmail.com>
2021-11-08 11:54:28 +0200

    Modified Routers so / redirects to home and layout appears on all

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 
 import Clients from './components/Clients';
 import ClientForm from './components/clientForm';
+import Layout from './components/layout';
+import Home from './components/home';
+
 import './App.css';
 
 function App() {
   return (
     <main className='container'>
       <Routes>
-        <Route path='/clients' element={<Clients/>}/>
-        <Route path='/clients/:id' element={<ClientForm/>}/>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='clients' element={<Clients />} />
+          <Route path='clients/:id' element={<ClientForm />} />
+        </Route>
       </Routes>
     </main>
   );
